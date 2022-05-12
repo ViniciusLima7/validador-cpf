71c291d7bdf2c559d82a92bbc0c234c54d530fc1
Marcos Vinicius Lima de França <mfranca@nelogica.com.br>
2022-05-11 22:01:48 -0300

    Corrige Validação de CPF e adiciona verificação por Estado

--- script.js
+++ script.js
@@ -7,37 +7,88 @@ console.log("Javascript Carregado");
  */
 
 function validarCPF(cpf) {
-  if (cpf.length !== 11) {
+  //Remove mascara
+  // cpf = cpf.replace(/[^\d]+/g, "");
+
+  if (
+    cpf.length !== 11 ||
+    cpf === "00000000000" ||
+    cpf === "11111111111" ||
+    cpf === "22222222222" ||
+    cpf === "33333333333" ||
+    cpf === "44444444444" ||
+    cpf === "55555555555" ||
+    cpf === "66666666666" ||
+    cpf === "77777777777" ||
+    cpf === "88888888888" ||
+    cpf === "99999999999"
+  ) {
     return false;
-  } else {
-    var numerosCPF = cpf.substring(0, 9);
-    var digitoCPF = cpf.substring(9);
-    var soma = 0;
-
-    for (var i = 10; i > 1; i--) {
-      soma += numerosCPF.charAt(10 - i) * i;
-    }
-
-    var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
-
-    // Validação do Primeiro Digito
-    if (resultado != digitoCPF.charAt(0)) {
-      return false;
-    }
-
-    soma = 0;
-    numerosCPF = cpf.substring(0, 10);
-    for (var k = 11; k > 1; k--) {
-      soma += numerosCPF.charAt(11 - k) * k;
-    }
-    resultado = soma % 11 > 2 ? 0 : 11 - (soma % 11);
-
-    // Validação do Segundo Digito
-    if (resultado != digitoCPF.charAt(1)) {
-      return false;
-    }
-    return true;
   }
+  var soma = 0;
+
+  //Soma os 9 primeiros Digitos multiplicados de 10 a 2
+  for (var i = 0; i < 9; i++) {
+    soma += cpf.charAt(i) * (10 - i);
+  }
+
+  // Validação do Primeiro Digito
+  var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+  if (resultado != cpf.charAt(9)) {
+    return false;
+  }
+
+  soma = 0;
+
+  //Soma os 10 primeiros Digitos multiplicados de 11 a 2
+  for (var k = 0; k < 10; k++) {
+    soma += cpf.charAt(k) * (11 - k);
+  }
+
+  // Validação do Segundo Digito
+  resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+  if (resultado != cpf.charAt(10)) {
+    return false;
+  }
+
+  //Valida Origem do CPF
+  switch (cpf.charAt(8)) {
+    case "1":
+      console.log(
+        "CPF é do Distrito Federal, Goiás, Mato Grosso do Sul ou Tocantins"
+      );
+      break;
+    case "2":
+      console.log("CPF é do Pará, Amazonas, Acre, Amapá, Rondônia ou Roraima");
+      break;
+    case "3":
+      console.log("CPF é do Ceará, Maranhão ou Piau");
+      break;
+    case "4":
+      console.log(
+        "CPF é do Pernambuco, Rio Grande do Norte, Paraíba ou Alagoas"
+      );
+      break;
+    case "5":
+      console.log("CPF é da Bahia ou Sergipe");
+      break;
+    case "6":
+      console.log("CPF é de Minas Gerais");
+      break;
+    case "7":
+      console.log("CPF é do Rio de Janeiro ou Espírito Santo");
+      break;
+    case "8":
+      console.log("CPF é de São Paulo");
+      break;
+    case "9":
+      console.log("CPF é do Paraná ou Santa Catarina");
+      break;
+    case "0":
+      console.log("CPF é do Rio Grande do Sul");
+      break;
+  }
+  return true;
 }
 
 /**
